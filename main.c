f22ac43ebc2d1bc7d1b8eb190b3fc47db4cc2d05
lee eunji <eunjizz97@sookmyung.ac.kr>
2018-10-11 16:05:56 +0900

    문자 개수 세기

--- main.c
+++ main.c
@@ -5,33 +5,23 @@
 
 int main(int argc, char *argv[]) {
 	
-	int month;
-	int day;		//일수를 계산하는 변수 
+	int num = 0;		//숫자의 개수 
+	char str[100];
+	char c;
 	
-	printf("input the month : ");
-	scanf("%i", &month);
+	//string
+	printf("input a string : ");
 	
-	switch (month) {
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			day = 31;
-			break;
+	while( (c=getchar() )!='\n')
+	{
+		if ('0'<=c && c<='9')
+			num = num + 1;
 			
-		case 2:
-			day = 28;
-			break;
-			
-		default:
-			day = 30;
-			break;
+		else
+			num = num + 0;
 	}
-		
-	printf("the day is %d", day);
+	
+	printf("the number of digits is %d\n", num);
 	
 	return 0;
 }
